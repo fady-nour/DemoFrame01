72a501c9ebd8e7a57662c421e7f42b45d69db698
Fadynour <fadynour194@gmail.com>
2025-09-14 18:56:57 +0300

    all

--- DemoFrame01/Context/ComponeyDbcontext.cs
+++ DemoFrame01/Context/ComponeyDbcontext.cs
@@ -30,8 +30,53 @@ namespace DemoFrame01.Context
         #region apply floating apis
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Employee>().HasOne(E => E.Manager).WithOne(E => E.ManagedDep)
-               .HasForeignKey<Department>(E=>E.ManagerId).OnDelete(DeleteBehavior.NoAction) ;
+
+            //modelBuilder.Entity<Employee>().HasOne(E => E.Manager).WithOne(E => E.ManagedDep)
+            //   .HasForeignKey<Department>(E=>E.ManagerId).OnDelete(DeleteBehavior.NoAction) ;
+            #region Assignment02
+            modelBuilder.Entity<Student>()
+       .HasOne<Department>()                    
+       .WithMany()                                
+       .HasForeignKey(s => s.Id);
+            modelBuilder.Entity<Instructor>()
+                .HasOne<Department>()
+                .WithMany()
+                .HasForeignKey(i => i.ID);
+            modelBuilder.Entity<Course>()
+       .HasOne<Topic>()                           
+       .WithMany()                              
+       .HasForeignKey(c => c.Top_ID)
+       .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<Stud_Course>()
+       .HasKey(sc => new { sc.stud_ID, sc.Course_ID });
+
+            modelBuilder.Entity<Stud_Course>()
+                .HasOne<Student>()
+                .WithMany()
+                .HasForeignKey(sc => sc.stud_ID)
+                ;
+
+            modelBuilder.Entity<Stud_Course>()
+                .HasOne<Course>()
+                .WithMany()
+                .HasForeignKey(sc => sc.Course_ID)
+               ;
+
+            modelBuilder.Entity<Course_Inst>()
+    .HasKey(ci => new { ci.inst_ID, ci.Course_ID });
+
+            modelBuilder.Entity<Course_Inst>()
+                .HasOne<Instructor>()
+                .WithMany()
+                .HasForeignKey(ci => ci.inst_ID)
+               ;
+
+            modelBuilder.Entity<Course_Inst>()
+                .HasOne<Course>()
+                .WithMany()
+                .HasForeignKey(ci => ci.Course_ID)
+             ;
+            #endregion
         }
         #endregion
         public DbSet<Employee> Employees { get; set; }
